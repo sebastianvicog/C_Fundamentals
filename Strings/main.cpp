6dbbd58736c68c24f74fb7607d268cf188317658
sebastianvicog <sebastianvicog@gmail.com>
2023-12-14 12:42:39 +0100

    Strings v1: Class created.

---
+++ Strings/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Sebastián Alberto Vico Guzmán
+//
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+// [1.] Introduction: char
+/*
+    char n1[] = "Sebastian";
+    cout << n1 << endl;
+*/
+
+// [2.] Save a user inserted char
+/*
+    char n2[10];
+    cout << "Insert your name: ";
+    //cin >> n2; //First method
+    //gets(n2); //Second method
+    cin.getline(n2, 20, '\n');
+
+    cout << "\nDisplay your name: " << n2 << endl;
+*/
+
+
+//[3.]
+
+
+    return 0;
+}
