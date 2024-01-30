6b938bed387e5f7a279a89cbb57fa3493d19f995
sebastianvicog <sebastianvicog@gmail.com>
2024-01-30 10:50:29 +0100

    Class SortingAlgorithms created

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -52,6 +52,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/sebav/CLionProjects/ATS_Course/cmake-build-debug/Strings/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/sebav/CLionProjects/ATS_Course/cmake-build-debug/SortingAlgorithms/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
