25cd817c939089e9edf75439a1eb4954499cc93f
sebastianvicog <sebastianvicog@gmail.com>
2024-02-05 19:58:18 +0100

    Srtruct example2. Nested structures

--- Struct/main.cpp
+++ Struct/main.cpp
@@ -2,10 +2,11 @@
 // Created by Sebastián Vico Guzmán
 //
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-//Example of the use of a structure. Declaration here.
+// Example 1. Use of a structure. Declaration here.
 struct Person {
     char name[20];
     int age;
@@ -14,9 +15,23 @@ person1 = {"Sebastian", 23},
 person2 = {"Alex", 15},
 person3;
 
+// Example 2. Nested structures
+struct AddressInformation {
+    char address[30];
+    char city[20];
+    char province[20];
+};
+
+struct Employee {
+    char name[20];
+    struct AddressInformation employeeAddress; // Here
+    double salary;
+} employees[2]; // STRUCT ARRAY
+
 
 int main(){
-    //Example of the use of a structure. Declaration in the struct.
+/*
+//Example 1. Use of a structure. Declaration in the struct.
     cout << "Name1: " << person1.name << endl;
     cout << "Age1: " << person1.age << endl;
     cout << "Name2: " << person2.name << endl;
@@ -34,5 +49,33 @@ int main(){
 
     cout << "Name4: " << person4.name << endl;
     cout << "Age4: " << person4.age << endl;
+*/
+
+// Example 2. Nested structures
+    //Filling data:
+    for (int i = 0; i < 2; i++){
+        cout << "Type your name: ";
+        cin.getline(employees[i].name, 20, '\n');
+        cout << "Type your address: ";
+        cin.getline(employees[i].employeeAddress.address, 30, '\n');
+        cout << "Type your city: ";
+        cin.getline(employees[i].employeeAddress.city, 20, '\n');
+        cout << "Type your province: ";
+        cin.getline(employees[i].employeeAddress.province, 20, '\n');
+        cout << "Type your salary: ";
+        cin >> employees[i].salary;
+        cout << endl;
+    }
+
+    //Printing data:
+    for (int i = 0; i < 2; i++){
+        fflush(stdin); // Empty the buffer and enable typing of values
+        cout << "Name: " << employees[i].name;
+        cout << " Address: " << employees[i].employeeAddress.address;
+        cout << " City: " << employees[i].employeeAddress.city;
+        cout << " Province: " << employees[i].employeeAddress.province;
+        cout << " Salary: " << employees[i].salary;
+        cout << endl;
+    }
 
 }
