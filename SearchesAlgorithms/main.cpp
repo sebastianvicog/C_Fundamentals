c14e2d564d3cffda2ec64df5e5ec8e38d6424bde
sebastianvicog <sebastianvicog@gmail.com>
2024-02-05 18:40:52 +0100

    Binary Search

--- SearchesAlgorithms/main.cpp
+++ SearchesAlgorithms/main.cpp
@@ -28,13 +28,93 @@ void sequentialSearch(const int v[], int size, int data){
 }
 
 /**
+ * @brief Swap Function
+ *
+ * This function is necessary for the Quick sort function
+ *
+ * @param x
+ * @param y
+ */
+void swapFunction(int &x, int &y){
+    int aux;
+
+    aux = x;
+    x = y;
+    y = aux;
+}
+
+/**
+ * @brief Quick Sort
+ *
+ * This function is necessary for sort the vector. Is required in the binary search.
+ *
+ * @param vector
+ * @param initialPosition
+ * @param finalPosition
+ */
+void quickSort (int vector[], int initialPosition, int finalPosition){
+    int i = initialPosition, j = finalPosition;
+    int middlePosition, pivot;
+
+    middlePosition = (initialPosition + finalPosition) / 2;
+    pivot = vector[middlePosition];
+
+    do {
+        while (vector[i] < pivot ) i++; // Iterator i advances
+        while (vector[j] > pivot) j--; // Iterator j goes backwards
+
+        if (i <= j){
+            swapFunction(vector[i], vector[j]); // Swap data
+            i++;
+            j--;
+        }
+
+    } while(i <= j); // Stopping condition: i == j or j > i
+
+    if (initialPosition < j){
+        quickSort(vector, initialPosition, j); // left subset recursive sorting
+    }
+
+    if (i < finalPosition){
+        quickSort(vector, i, finalPosition); // right subset recursive sorting
+    }
+}
+
+/**
  * @brief Binary Search
+ *
+ * It is necessary that the vector be ordered
+ *
  * @param v
  * @param size
  * @param data
  */
 void binarySearch(const int v[], int size, int data){
+    char flat = 'F';
+    int lower = 0, upper = size, middle;
 
+    while (lower <= upper){
+        middle = (lower + upper)/2;
+
+        if (v[middle] == data){
+            flat = 'T';
+            break;
+        }
+        if (v[middle] > data){
+            upper = middle;
+            middle = (lower + upper)/2;
+        }
+        if (v[middle] < data){
+            lower = middle;
+            middle = (lower + upper)/2;
+        }
+    }
+
+    if (flat == 'F'){
+        cout << endl << "- The number is not in the array" << endl;
+    } else {
+        cout << endl << "- The number is in the array. Position: " << middle << endl;
+    }
 }
 
 /**
@@ -76,7 +156,10 @@ int main(){
                 sequentialSearch(vNumbers, size, data);
                 break;
             case 2:
-                cout << "Binary Search in a vector ";
+                cout << "Ordered vector: ";
+                quickSort(vNumbers, 0,size);
+                printVector(vNumbers, size);
+                cout << endl << "Binary Search in a vector ";
                 binarySearch(vNumbers, size, data);
                 break;
             default:
